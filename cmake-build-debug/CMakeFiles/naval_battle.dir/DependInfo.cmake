0419c4333b487aa95cd2dab375da94ff91236a73
SunoWhere <sunowhere@gmail.com>
2021-04-16 11:55:04 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/naval_battle.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/SunoWhere/CLionProjects/naval_battle/src/game_initialization.c" "C:/Users/SunoWhere/CLionProjects/naval_battle/cmake-build-debug/CMakeFiles/naval_battle.dir/src/game_initialization.c.obj"
+  "C:/Users/SunoWhere/CLionProjects/naval_battle/src/main.c" "C:/Users/SunoWhere/CLionProjects/naval_battle/cmake-build-debug/CMakeFiles/naval_battle.dir/src/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "game_initialization.h"
+
+int main() {
+
+    return 0;
+}
