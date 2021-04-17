f7f933cf2da788d4c22926b12fbee15b0556fd1c
SunoWhere <sunowhere@gmail.com>
2021-04-18 01:00:09 +0200

    Update 1

---
+++ inc/struct.h
@@ -0,0 +1,27 @@
+#ifndef NAVAL_BATTLE_STRUCT_H
+#define NAVAL_BATTLE_STRUCT_H
+
+#define H 0
+#define V 1
+
+typedef struct{
+    unsigned short height;
+    unsigned short width;
+    char **boat_array;
+}Grid;
+
+typedef struct{
+    unsigned short artillery;
+    unsigned short tactical;
+    unsigned short bomb;
+    unsigned short simple_missile;
+}Inventory;
+
+typedef struct {
+    unsigned short position[2];
+    unsigned short size;
+    unsigned short orientation;
+}Boat;
+
+
+#endif //NAVAL_BATTLE_STRUCT_H
