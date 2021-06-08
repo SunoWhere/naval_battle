f069c4ae4f2fc86feaabf599e41345b75e63250a
SunoWhere <sunowhere@gmail.com>
2021-06-08 21:38:40 +0200

    Comment

--- src/display.c
+++ src/display.c
@@ -1,8 +1,8 @@
 #include "display.h"
 
 /**
- *
- * @param grid
+ * Affiche la grille de jeu avec les bateaux dissimulés et les marqueurs liés aux impacts
+ * @param grid - La grille de Jeu
  */
 void show_grid(Grid *grid){
     printf("   | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |\n");
@@ -29,8 +29,8 @@ void show_grid(Grid *grid){
 }*/
 
 /**
- *
- * @param inventory
+ * Affiche l'inventaire du joueur avec la quantité restante de chaque arme
+ * @param inventory - Inventaire du joueur
  */
 void show_inventory(Inventory *inventory){
     printf("Inventory :\n"
@@ -42,9 +42,9 @@ void show_inventory(Inventory *inventory){
 }
 
 /**
- *
- * @param difficulty
- * @return
+ * Récupère la difficulté (la valeur en tant qu'énumération de type Difficulty)
+ * @param difficulty - Difficulté actuelle
+ * @return Retourne la difficulté sour la forme d'une chaîne de charactères
  */
 char *difficulty_str(Difficulty difficulty){
     switch (difficulty) {
@@ -58,9 +58,9 @@ char *difficulty_str(Difficulty difficulty){
 }
 
 /**
- *
- * @param gamemode
- * @return
+ * Récupère le mode de jeu (la valeur en tant qu'énumération de type Mode)
+ * @param gamemode - Mode de jeu actuel
+ * @return Retourne le mode de jeu sour la forme d'une chaîne de charactères
  */
 char *gamemode_str(Mode gamemode){
     switch (gamemode) {
@@ -74,9 +74,9 @@ char *gamemode_str(Mode gamemode){
 }
 
 /**
- *
- * @param grid
- * @param fleet
+ * Affiche la liste des bateaux restant avec leur point de vie respectif
+ * @param grid - Grille de jeu
+ * @param fleet - Flotte de bateaux dont on veut afficher le status
  */
 void show_remaining_boat(Grid *grid, Boat *fleet){
     printf("Remaining boat in fleet : %d\n", remaining_boat(grid, fleet));
@@ -89,9 +89,10 @@ void show_remaining_boat(Grid *grid, Boat *fleet){
 }
 
 /**
- *
- * @param difficulty
- * @param gamemode
+ * Affiche la difficulté et le mode jeu actuelle à partir des valeurs des énum en paramètre, le nom de la difficulté et
+ * du mode de jeu sont récupérés grâce aux fonction difficulty_str et gamemode_str()
+ * @param difficulty - Difficulté actuelle
+ * @param gamemode - Mode de jeu actuel
  */
 void show_difficulty_gamemode(Difficulty difficulty, Mode gamemode){
     printf("Difficulty = %s, Gamemode = %s\n", difficulty_str(difficulty), gamemode_str(gamemode));
@@ -99,12 +100,12 @@ void show_difficulty_gamemode(Difficulty difficulty, Mode gamemode){
 }
 
 /**
- *
- * @param grid
- * @param inventory
- * @param difficulty
- * @param gamemode
- * @param fleet
+ * Appelle les fonctions utiles à l'affichage du mode CLASSIC
+ * @param grid - Grille de jeu
+ * @param inventory - Inventaire du joueur
+ * @param difficulty - Difficulté actuelle
+ * @param gamemode - Mode de jeu actuel
+ * @param fleet - Flotte contenant les bateaux ennemis
  */
 void show_classic(Grid *grid, Inventory *inventory, Difficulty difficulty, Mode gamemode, Boat *fleet){
     show_difficulty_gamemode(difficulty, gamemode);
@@ -114,12 +115,12 @@ void show_classic(Grid *grid, Inventory *inventory, Difficulty difficulty, Mode 
 }
 
 /**
- *
- * @param grid
- * @param inventory
- * @param difficulty
- * @param gamemode
- * @param fleet
+ * Appelle les fonctions utiles à l'affichage du mode BLIND
+ * @param grid - Grille de jeu (la grille n'est pas affichée mais nécessaire pour les autres fonctions)
+ * @param inventory - Inventaire du joueur
+ * @param difficulty - Difficulté actuelle
+ * @param gamemode - Mode de jeu actuel
+ * @param fleet - Flotte contenant les bateaux ennemis
  */
 void show_blind(Grid *grid, Inventory *inventory, Difficulty difficulty, Mode gamemode, Boat *fleet){
     show_difficulty_gamemode(difficulty, gamemode);
@@ -128,13 +129,14 @@ void show_blind(Grid *grid, Inventory *inventory, Difficulty difficulty, Mode ga
 }
 
 /**
- *
- * @param grid
- * @param grid_displayed_active
- * @param inventory
- * @param difficulty
- * @param gamemode
- * @param fleet
+ * Appelle les fonctions utiles à l'affichage du mode ACTIVE
+ * @param grid - Grille de jeu (cette grille n'est pas affichée mais contient des informations importantes)
+ * @param grid_displayed_active - Grille de jeu à afficher (cette grille est affichée et contient uniquement les
+ * informations nécessaire pour l'affichage)
+ * @param inventory - Inventaire du joueur
+ * @param difficulty - Difficulté actuelle
+ * @param gamemode - Mode de jeu actuel
+ * @param fleet - Flotte contenant les bateaux ennemis
  */
 void show_active(Grid *grid, Grid *grid_displayed_active, Inventory *inventory, Difficulty difficulty, Mode gamemode, Boat *fleet){
     show_difficulty_gamemode(difficulty, gamemode);
